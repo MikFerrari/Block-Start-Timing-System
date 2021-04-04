1885e1b91f89fddf8af42dd3555565a2aa3cf21b
MikFerrari <m.ferrari092@studenti.unibs.it>
2021-04-04 15:59:33 +0200

    first version of master and slave code

--- src/finishSlave/finishSlave.ino
+++ src/finishSlave/finishSlave.ino
@@ -1,9 +1,44 @@
-void setup() {
-  // put your setup code here, to run once:
+// Import library for HC-12 Radio Module
+#include <SoftwareSerial.h>
 
+// Variable declarations
+SoftwareSerial HC12(2, 3); // HC-12 TX Pin, HC-12 RX Pin
+
+const int photocellPin = 6;
+int photocellStatus;
+
+bool athleteRunning = false;
+
+float initialTime = 0;
+float totalTime = 0;
+
+
+void setup() {
+  // Initialise Serial communication and Radio Module
+  Serial.begin(9600);
+  HC12.begin(9600);
+
+  // Declare starting block pin
+  pinMode(photocellPin,INPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
+  // Read block status to determine if the athlete is ready to start
+  photocellStatus = digitalRead(photocellPin);
+
+  // Receive start signal from master
+  while(HC12.available() && athleteRunning == false) {
+    initialTime = millis();
+    athleteRunning = true;
+  }
+
+  if(photocellStatus == 0 && athleteRunning == true) {
+    totalTime = millis() - initialTime;
+    athleteRunning = false;
+    // Send to master total elapsed time
+    HC12.write(totalTime);
+  }
+  
+  Serial.println(photocellStatus); // For debugging
 }
--- src/startMaster/startMaster.ino
+++ src/startMaster/startMaster.ino
@@ -1,9 +1,59 @@
-void setup() {
-  // put your setup code here, to run once:
+// Import libraries for LCD Display and HC-12 Radio Module
+#include <SoftwareSerial.h>
+#include <LiquidCrystal.h>
 
+// Variable declarations
+SoftwareSerial HC12(2, 3); // HC-12 TX Pin, HC-12 RX Pin
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+
+const int blockPin = 6;
+int blockStatus;
+
+bool athleteReady = false;
+bool athleteRunning = false;
+
+float totalTime = 0;
+
+
+void setup() {
+  // Set up the LCD's number of columns and rows
+  lcd.begin(16, 2);
+
+  // Initialise Serial communication and Radio Module
+  Serial.begin(9600);
+  HC12.begin(9600);
+
+  // Declare starting block pin
+  pinMode(blockPin,INPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
+  // Read block status to determine if the athlete is ready to start
+  blockStatus = digitalRead(blockPin);
+
+  if(blockStatus == 0) {
+    athleteReady = true;
+  }
+
+  if(athleteReady == true && blockStatus == 1) {
+    HC12.write("start");
+    athleteReady = false;
+    athleteRunning = true;  
+  }
+
+  // Receive total elapsed time measured by the slave
+  while(HC12.available()) {
+    totalTime = HC12.read();
+    Serial.write(totalTime); // For debugging
+    athleteRunning = false;
+  }
+
+  // Print time on LCD Display  
+  lcd.setCursor(0, 0); // Time computed by the master board
+  lcd.print("Total time [s]:");
+  lcd.setCursor(0, 1);
+  lcd.print(totalTime/1000);
+
+  // Serial.println(blockStatus); // For debugging
 }
